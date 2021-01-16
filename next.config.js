2eb6ac762a566c35016dfb0bfa87cde5a7327797
Kevin Childs <kchilds2020@gmail.com>
2021-01-16 00:22:15 -0600

    temp

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    env:{
-        MONGO_URI: "mongodb+srv://kchilds2020:Gertie2018@kevdevcluster.y02xc.mongodb.net/test?authSource=admin&replicaSet=atlas-c2tkbc-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-    }
-}
